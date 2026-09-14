549af01a5ff45b493b4aedfd36bb16e0e3391e88
Randomz4-debug <shresthmehta64@gmail.com>
2026-09-14 16:31:55 +0530

    Fix Android Java/Kotlin JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
